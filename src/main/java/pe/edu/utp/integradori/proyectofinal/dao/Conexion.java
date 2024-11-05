a7c2b717ba7b4f4149cba64d78c592e6fb25e18f
Joaquin Requejo <joaquinrequejo18@gmail.com>
2024-11-05 11:37:21 -0500

    Agregado log al acceder a la base de datos

--- src/main/java/pe/edu/utp/integradori/proyectofinal/dao/Conexion.java
+++ src/main/java/pe/edu/utp/integradori/proyectofinal/dao/Conexion.java
@@ -1,12 +1,16 @@
 package pe.edu.utp.integradori.proyectofinal.dao;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class Conexion {
+
+    Logger logger = LoggerFactory.getLogger(Conexion.class);
 
     protected Connection getConnection() throws SQLException {
         Dotenv dotenv = Dotenv.load();
@@ -18,6 +22,7 @@ public abstract class Conexion {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+            logger.error("No se pudo acceder a la base de datos", e);
             throw new SQLException("No se pudo cargar el driver JDBC", e);
         }
 
